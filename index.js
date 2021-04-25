f5dced4a8a75913acfc46268ea98d33fa8bfb880
hugoRoca <hugo.rock20@hotmail.com>
2021-04-25 17:42:45 -0500

    finish

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const serverless = require('serverless-http')
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express()
 
 app.use(bodyParser.json())
+
+// TODO: Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // TODO: Routes
 app.use('/course', require('./src/routes/course.route'))
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Integration SWAPI and dynamodb
+
+Steps for install:
+1. run `npm install`
+2. config your credential of aws
+3. run `sls deploy`
+
+![swagger_doc](doc/deploy.png)
+
+For exec unit test: `npm run test`
+
+the endpoints this repository are bellow: `/dev/api-docs`
+
+![swagger_doc](doc/swagger.png)
+
+For this project has the architect bellow:
+
+![graphic](doc/graphic.png)
+
+
